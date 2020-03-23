7a2ffdb2e74f383f3db85ae6dd29c397b1fc1475
Liron-Wassa <khruiuuxv@gmail.com>
2020-03-23 19:13:35 +0200

    Updates

--- precache-manifest.6f8f5616d85e702cc5c01c115d0b43fd.js
+++ precache-manifest.6f8f5616d85e702cc5c01c115d0b43fd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1be0d3f073dfd691c9a57af69e65997e",
+    "revision": "35a531b345e523165b1129942918a2c0",
     "url": "/Library/index.html"
   },
   {
-    "revision": "912bba4eee78788e3591",
+    "revision": "1497405f2b298ce81d06",
     "url": "/Library/static/css/main.eb9a289b.chunk.css"
   },
   {
-    "revision": "fd8084209b870c34bd89",
-    "url": "/Library/static/js/2.81809596.chunk.js"
+    "revision": "7f1043a2fc4e55cfa82c",
+    "url": "/Library/static/js/2.8790861a.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/Library/static/js/2.81809596.chunk.js.LICENSE.txt"
+    "url": "/Library/static/js/2.8790861a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "912bba4eee78788e3591",
-    "url": "/Library/static/js/main.bc308a6c.chunk.js"
+    "revision": "1497405f2b298ce81d06",
+    "url": "/Library/static/js/main.651a553f.chunk.js"
   },
   {
     "revision": "b7acdd662619ca2dfdd0",
